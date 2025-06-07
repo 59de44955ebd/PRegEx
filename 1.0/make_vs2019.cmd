77fedf67f9fc26db5afd3a5862a5c2b072524574
59de44955ebd <59de44955ebd@gmail.com>
2025-06-07 09:46:43 +0200

    edited make scripts

--- 1.0/make_vs2019.cmd
+++ 1.0/make_vs2019.cmd
@@ -1,5 +1,6 @@
 @echo off
+del "%~dp0\xtras\Release\PRegEx.x32" 2>nul
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
-msbuild %~dp0\project\PRegEx.sln /p:Configuration=Release /p:Platform=Win32
+msbuild "%~dp0\project\PRegEx.sln" /p:Configuration=Release /p:Platform=Win32
 echo.
 pause
--- 2.0/make_vs2022_debug.cmd
+++ 2.0/make_vs2022_debug.cmd
@@ -1,5 +1,6 @@
 @echo off
+del "%~dp0\xtras\Debug\PRegEx.x32" 2>nul
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
-msbuild %~dp0\project\PRegEx.sln /p:Configuration=Debug /p:Platform=Win32
+msbuild "%~dp0\project\PRegEx.sln" /p:Configuration=Debug /p:Platform=Win32
 echo.
 pause
